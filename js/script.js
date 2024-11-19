6d0f1ba1723a5cd58578b7ee03b22c3a4fc64476
Heloisa <helo.esteralmeida@gmail.com>
2024-11-18 22:08:55 -0300

    javascript aula 17

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* PROCEDIMENTO
+Descrição: Exibe uma mensagem fixa 
+Nome: mostraAlerta
+Parâmetro: Void 
+Retorno: Void
+Void -> Significa que não retorna valor*/ 
+
+function mostraAlerta() 
+{
+    alert("Não é que isso funciona mesmo!!!! ");
+}
+
+/* PROCEDIMENTO
+Descrição: Exibe uma mensagem com parâmetro 
+Nome: mostraAlerta1
+Parâmetro: string
+Retorno: Void
+Void -> Significa que não retorna valor*/ 
+
+
+function mostraAlerta1(msg) 
+{
+    alert("Boa noite "+msg);
+}
+
+function mensagem_com_parametro_return(msg1)
+{
+    return("Você digitou: " + msg1);
+}
